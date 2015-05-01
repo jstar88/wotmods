2ba6b4dd61bb783799f85a531f68abe70d026fbe
jstar88 <covolo.n@libero.it>
2015-05-01 03:45:47 +0200

    Changed sixthsense sound reader. Added icons to sixthsense. Optimized Sixthsense.

--- files/uncompyled/wot_folder/res_mods/0.9.7/scripts/client/plugins/SixthSenseDuration_plugin/SixthSenseDuration.py
+++ files/uncompyled/wot_folder/res_mods/0.9.7/scripts/client/plugins/SixthSenseDuration_plugin/SixthSenseDuration.py
@@ -12,13 +12,14 @@ from gui import GUI_SETTINGS
 import re
 from plugins.Engine.ModUtils import BattleUtils,MinimapUtils,FileUtils,HotKeysUtils,DecorateUtils
 import subprocess
+import threading
+from CurrentVehicle import g_currentVehicle
 
 class SixthSenseDuration:
     myConf = {
               'AudioPath': '/GUI/notifications_FX/cybersport_timer',
               'AudioIsExternal': True,
-              'AudioExternal': ['res_mods/{v}/scripts/client/plugins/SixthSenseDuration_plugin/resources/dlc.exe',
-                                '-p',
+              'AudioExternal': ['res_mods/{v}/scripts/client/plugins/SixthSenseDuration_plugin/resources/cmdmp3win.exe',
                                 'res_mods/{v}/scripts/client/plugins/SixthSenseDuration_plugin/resources/sound.mp3'],
               'AudioRange': 9000,
               'AudioTick': 1000,
@@ -32,10 +33,21 @@ class SixthSenseDuration:
               'TimerTick': 1000,
               'TimerZeroDelay': 200,
               'TimerText': "%s",
-              'pluginEnable' : True
+              'pluginEnable' : True,
+              'DisplayOriginalIcon': False,
+              'IconInactivePosition': "(round(x / 2) + 120,  20, 0.7)",
+              'IconUnspottedPosition': "(round(x / 2) + 120, 20, 0.7)",
+              'IconSpottedPosition': "(round(x / 2) + 120, 20, 0.7)",
+              'IconInactivePath': "scripts/client/plugins/SixthSenseDuration_plugin/resources/inactive.dds",
+              'IconUnspottedPath': "scripts/client/plugins/SixthSenseDuration_plugin/resources/unspotted.dds",
+              'IconSpottedPath': "scripts/client/plugins/SixthSenseDuration_plugin/resources/spotted.dds"
     }
     guiCountDown = None
     backupVolume = None 
+    hasSixthSense = False
+    guiUnspotted = None
+    guiInactive = None
+    guiSpotted = None
     
     
     def __init__(self):
@@ -56,15 +68,21 @@ class SixthSenseDuration:
     def sequenceEnd():
         SoundGroups.g_instance.setVolume(SixthSenseDuration.myConf['VolumeType'],SixthSenseDuration.backupVolume)
     
+    
+    @staticmethod
+    def threadedPlayExtSound():
+        startupinfo = subprocess.STARTUPINFO()
+        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        startupinfo.wShowWindow = subprocess.SW_HIDE
+        subprocess.Popen(SixthSenseDuration.myConf['AudioExternal'], startupinfo=startupinfo)
+    
     @staticmethod
     def playExtSound(i):
         if i < 1:
             return
         try:
-            startupinfo = subprocess.STARTUPINFO()
-            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-            startupinfo.wShowWindow = subprocess.SW_HIDE
-            subprocess.Popen(SixthSenseDuration.myConf['AudioExternal'], startupinfo=startupinfo)
+            t = threading.Thread(target=SixthSenseDuration.threadedPlayExtSound)
+            t.start()
         except:
             pass
         i -= 1
@@ -72,7 +90,16 @@ class SixthSenseDuration:
     
     @staticmethod
     def new_showSixthSenseIndicator(self, isShow):
-        old_showSixthSenseIndicatorFromSixthSenseDuration(self, isShow)
+        if SixthSenseDuration.myConf['DisplayOriginalIcon']:
+            old_showSixthSenseIndicatorFromSixthSenseDuration(self, isShow)
+            
+        if SixthSenseDuration.hasSixthSense:
+            SixthSenseDuration.initGuiSpotted()
+            SixthSenseDuration.initGuiUnspotted()
+            SixthSenseDuration.guiSpotted.visible = isShow
+            SixthSenseDuration.guiUnspotted.visible = not isShow
+            BigWorld.callback(SixthSenseDuration.myConf['IconRange'],self.invertIcons())
+            
         SixthSenseDuration.startGuiCountDown()
         
         i = SixthSenseDuration.myConf['AudioRange'] / SixthSenseDuration.myConf['AudioTick']
@@ -84,7 +111,27 @@ class SixthSenseDuration:
         else:
             SixthSenseDuration.playExtSound(i)
 
+    @staticmethod
+    def invertIcons():
+        SixthSenseDuration.guiSpotted.visible = not SixthSenseDuration.guiSpotted.visible
+        SixthSenseDuration.guiUnspotted.visible = not SixthSenseDuration.guiUnspotted.visible
+    
     # --------------- icon ------------- #
+    @staticmethod
+    def onChangedVeh():
+        for c in g_currentVehicle.item.crew:
+            tankcrew = c[1]
+            if tankcrew is None:
+                continue
+            skills = tankcrew.skillsMap
+            if 'commander_sixthSense' in skills:
+                commander_sixthSense = skills['commander_sixthSense']
+                if commander_sixthSense.isActive and commander_sixthSense.isEnable:
+                    SixthSenseDuration.hasSixthSense = True
+                    return
+        SixthSenseDuration.hasSixthSense = False
+    
+    
     @staticmethod
     def new_spaceDone(self):
         old_spaceDoneFromSixthSenseDuration(self)
@@ -96,6 +143,7 @@ class SixthSenseDuration:
     def initGuiCountDown():
         if SixthSenseDuration.guiCountDown is not None:
             return
+        SixthSenseDuration.fixPosition('TimerPosition')
         SixthSenseDuration.guiCountDown = GUI.Text('')
         GUI.addRoot(SixthSenseDuration.guiCountDown)
         SixthSenseDuration.guiCountDown.widthMode = 'PIXEL'
@@ -103,11 +151,46 @@ class SixthSenseDuration:
         SixthSenseDuration.guiCountDown.verticalPositionMode = 'PIXEL'
         SixthSenseDuration.guiCountDown.horizontalPositionMode = 'PIXEL'
         SixthSenseDuration.guiCountDown.horizontalAnchor = 'LEFT'
+        SixthSenseDuration.guiCountDown.verticalAnchor = 'TOP'
         SixthSenseDuration.guiCountDown.colourFormatting = True
         SixthSenseDuration.guiCountDown.font = SixthSenseDuration.myConf['TimerFont']
         SixthSenseDuration.guiCountDown.visible = False
-        x, y = GUI.screenResolution()
-        SixthSenseDuration.guiCountDown.position = eval(SixthSenseDuration.myConf['TimerPosition'])
+        SixthSenseDuration.guiCountDown.position = SixthSenseDuration.myConf['TimerPosition']
+        
+    @staticmethod
+    def initGuiSpotted():
+        if SixthSenseDuration.guiSpotted is None:
+            SixthSenseDuration.fixPosition('IconSpottedPosition')
+            SixthSenseDuration.guiSpotted = SixthSenseDuration.createTexture(SixthSenseDuration.myConf['IconSpottedPath'], SixthSenseDuration.myConf['IconSpottedPosition'])
+        SixthSenseDuration.guiSpotted.visible = False
+        
+    @staticmethod
+    def initGuiUnspotted():
+        if SixthSenseDuration.guiUnspotted is None:
+            SixthSenseDuration.fixPosition('IconUnspottedPosition')
+            SixthSenseDuration.guiUnspotted = SixthSenseDuration.createTexture(SixthSenseDuration.myConf['IconUnspottedPath'], SixthSenseDuration.myConf['IconUnspottedPosition'])
+        SixthSenseDuration.guiUnspotted.visible = SixthSenseDuration.hasSixthSense    
+            
+    @staticmethod
+    def initGuiInactive():
+        if SixthSenseDuration.guiInactive is None:
+            SixthSenseDuration.fixPosition('IconInactivePosition')
+            SixthSenseDuration.guiInactive = SixthSenseDuration.createTexture(SixthSenseDuration.myConf['IconInactivePath'], SixthSenseDuration.myConf['IconInactivePosition'])
+        SixthSenseDuration.guiInactive.visible = not SixthSenseDuration.hasSixthSense
+
+    @staticmethod
+    def createTexture(texture,position):
+        item = GUI.Simple(texture)
+        GUI.addRoot(item)
+        item.widthMode = 'PIXEL'
+        item.heightMode = 'PIXEL'
+        item.verticalPositionMode = 'PIXEL'
+        item.horizontalPositionMode = 'PIXEL'
+        item.horizontalAnchor = 'LEFT'
+        item.verticalAnchor = 'TOP'
+        item.position = position
+        item.size = (52,52)
+        return item
     
     @staticmethod
     def startGuiCountDown():
@@ -126,6 +209,21 @@ class SixthSenseDuration:
     def endGuiCountDown():
         if SixthSenseDuration.guiCountDown is not None:
             SixthSenseDuration.guiCountDown.visible = False
+            
+    @staticmethod
+    def endGuiInactive():
+        if SixthSenseDuration.guiInactive is not None:
+            SixthSenseDuration.guiInactive.visible = False
+            
+    @staticmethod
+    def endGuiUnspotted():
+        if SixthSenseDuration.guiUnspotted is not None:
+            SixthSenseDuration.guiUnspotted.visible = False
+            
+    @staticmethod
+    def endGuiSpotted():
+        if SixthSenseDuration.guiSpotted is not None:
+            SixthSenseDuration.guiSpotted.visible = False
     
     @staticmethod    
     def tickGuiCountDown(i):
@@ -135,13 +233,24 @@ class SixthSenseDuration:
         if i == 0:
             BigWorld.callback(SixthSenseDuration.myConf['TimerZeroDelay']/1000,SixthSenseDuration.endGuiCountDown )
 
+    @staticmethod
+    def fixPosition(type):
+        x, y = GUI.screenResolution()
+        SixthSenseDuration.myConf[type] = eval(SixthSenseDuration.myConf[type])
+    
     def readConfig(self):
         SixthSenseDuration.myConf = FileUtils.readConfig('scripts/client/plugins/SixthSenseDuration_plugin/config.xml',SixthSenseDuration.myConf,"SixthSenseDuration")
         self.pluginEnable =  SixthSenseDuration.myConf['pluginEnable']
+        wotv = FileUtils.getWotVersion()
         tmp = []
         for d in SixthSenseDuration.myConf['AudioExternal']:
-            tmp.append(d.replace('{v}',FileUtils.getWotVersion()))
+            tmp.append(d.replace('{v}',wotv))
         SixthSenseDuration.myConf['AudioExternal'] = tmp
+        SixthSenseDuration.myConf['IconInactivePath'] = SixthSenseDuration.myConf['IconInactivePath'].replace('{v}',wotv)
+        SixthSenseDuration.myConf['IconUnspottedPath'] = SixthSenseDuration.myConf['IconUnspottedPath'].replace('{v}',wotv)
+        SixthSenseDuration.myConf['IconSpottedPath'] = SixthSenseDuration.myConf['IconSpottedPath'].replace('{v}',wotv)
+        
+        
     
     def run(self):
         saveOldFuncs()
@@ -156,6 +265,13 @@ def saveOldFuncs():
     
 def injectNewFuncs():
     Battle.showSixthSenseIndicator = SixthSenseDuration.new_showSixthSenseIndicator
-    _HangarSpace._HangarSpace__spaceDone = SixthSenseDuration.new_spaceDone    
+    _HangarSpace._HangarSpace__spaceDone = SixthSenseDuration.new_spaceDone
     g_windowsManager.onInitBattleGUI += SixthSenseDuration.initGuiCountDown
+    g_windowsManager.onInitBattleGUI += SixthSenseDuration.initGuiInactive
+    g_windowsManager.onInitBattleGUI += SixthSenseDuration.initGuiSpotted
+    g_windowsManager.onInitBattleGUI += SixthSenseDuration.initGuiUnspotted
     g_windowsManager.onDestroyBattleGUI += SixthSenseDuration.endGuiCountDown
+    g_windowsManager.onDestroyBattleGUI += SixthSenseDuration.endGuiInactive
+    g_windowsManager.onDestroyBattleGUI += SixthSenseDuration.endGuiSpotted
+    g_windowsManager.onDestroyBattleGUI += SixthSenseDuration.endGuiUnspotted
+    g_currentVehicle.onChanged += SixthSenseDuration.onChangedVeh
