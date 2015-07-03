556e50d6c1f20c9b8bfd5a5049202f58def5cafd
jstar88 <covolo.n@libero.it>
2015-07-03 02:54:12 +0200

    new plugin spotExtended

---
+++ files/uncompyled/wot_folder/res_mods/0.9.8.1/scripts/client/plugins/SpotExtended_plugin/SpotExtended.py
@@ -0,0 +1,48 @@
+from gui.battle_control.battle_feedback import BattleFeedbackAdaptor
+from constants import BATTLE_EVENT_TYPE as _SET
+from gui.battle_control import g_sessionProvider
+from gui.battle_control.battlesessionprovider import BattleSessionProvider
+from gui.WindowsManager import g_windowsManager
+import BigWorld
+
+class SpotExtended:
+    myConf = {'pluginEnable' : True,
+              'panel':'PlayerMessagesPanel',
+              'color': 'gold'
+              }
+    
+    def __init__(self):
+        self.pluginEnable = False
+        
+    def run(self):
+        g_windowsManager.onInitBattleGUI += SpotExtended.start
+        BattleFeedbackAdaptor.setPlayerAssistResult = SpotExtended.setPlayerAssistResult
+        
+    def readConfig(self):
+        SpotExtended.myConf = FileUtils.readConfig('scripts/client/plugins/SpotExtended_plugin/config.xml',SpotExtended.myConf,"SpotExtended")
+        self.pluginEnable =  SpotExtended.myConf['pluginEnable']
+    
+
+    #Show message on specified panel: VehicleErrorsPanel, VehicleMessagesPanel, PlayerMessagesPanel
+    @staticmethod
+    def showMessageOnPanel(panel, msgText, color, index = 0):
+        if g_windowsManager.battleWindow is not None and panel in ('VehicleErrorsPanel', 'VehicleMessagesPanel', 'PlayerMessagesPanel'):
+            g_windowsManager.battleWindow.call('battle.' + panel + '.ShowMessage', [index, msgText, color])
+
+    @staticmethod
+    def start():
+        if g_sessionProvider is not None and g_sessionProvider.getFeedback() is not None:
+            g_sessionProvider._BattleSessionProvider__feedback.stop()
+            _sessionProvider._BattleSessionProvider__feedback = BattleFeedbackAdaptor()
+            g_sessionProvider._BattleSessionProvider__feedback.start(g_sessionProvider._BattleSessionProvider__arenaDP)
+
+    @staticmethod
+    def setPlayerAssistResult(self, assistType, vehiclesIDs):
+        if assistType == _SET.SPOTTED:
+            arena = BigWorld.player().arena
+            for idV in vehiclesIDs:
+                entryVehicle = arena.vehicles[idV]
+                vehicleDescr = entryVehicle['vehicleType']
+                vehicleType = vehicleDescr.type
+                shortName = vehicleType.shortUserString
+                SpotExtended.showMessageOnPanel(SpotExtended.myConf['panel'],shortName,SpotExtended.myConf['color'])
---
+++ files/uncompyled/wot_folder/res_mods/0.9.8.1/scripts/client/plugins/SpotExtended_plugin/__init__.py
@@ -0,0 +1,2 @@
+from SpotExtended import SpotExtended
+__all__=['SpotExtended']
