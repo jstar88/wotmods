79cc142340c04111a95cd8f82ba6cc23480b621f
jstar88 <covolo.n@libero.it>
2015-11-10 21:18:56 +0100

    is orgabot still working? :)

--- files/uncompyled/tests/b.py
+++ files/uncompyled/tests/b.py
@@ -16,6 +16,3 @@ def main(callback):
 
 
 B()
-# okay decompyling C:\Users\nicola user\wotmods\files\originals\tests\b.pyc 
-# decompiled 1 files: 1 okay, 0 failed, 0 verify failed
-# 2015.09.05 18:13:43 ora legale Europa occidentale
